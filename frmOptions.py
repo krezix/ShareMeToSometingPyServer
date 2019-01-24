d348c322e0cd283f3b9fcbf474134429512220bc
unknown <krezix@gmail.com>
2019-01-24 17:35:03 +0000

    frmOptions updates

--- frmOptions.py
+++ frmOptions.py
@@ -1,13 +1,46 @@
 import tkinter as tk
+import tkinter.ttk as ttk
+from tkinter import filedialog
 
 class frmOptions(tk.Frame):
     def __init__(self, parent, *args, **kwargs):
         tk.Frame.__init__(self, parent, *args, **kwargs)
         self.parent = parent
-        self.lblDefaultBrowser = tk.Label(self.parent,text="Default Browser :")
+        self.frmDefaultBrowser = tk.Frame(self)
+        
+        self.lblDefaultBrowser = tk.Label(self.frmDefaultBrowser,text="Default Browser :")
         self.lblDefaultBrowser.pack(side="left")
-        self.txtDefaultBrowser = tk.Entry(self.parent)
-        self.txtDefaultBrowser.pack()
+        #self.txtDefaultBrowser = tk.Entry(self)
+        #self.txtDefaultBrowser.pack(side="left", fill=tk.X,expand=True)
+        self.cbDefaultBrowser = tk.ttk.Combobox(self.frmDefaultBrowser, state="readonly",
+                                    values=[
+                                    "Mozilla Firefox", 
+                                    "Google Chrome",
+                                    "Internet Explorer",
+                                    "Safari",
+                                    "Opera"])
+        self.cbDefaultBrowser.current(0)
+        self.cbDefaultBrowser.bind("<<ComboboxSelected>>", self.OncbDefaulBrowserSelected) # for multiple combos use lambda event:self.OncbDefaulBrowserSelected(event, "Hello"))
+        self.cbDefaultBrowser.pack(side="left", fill=tk.X, expand=True)
+        self.frmDefaultBrowser.pack(fill=tk.X, ipadx=2, ipady=2)
+
+        self.frmBrowserLocation = tk.Frame(self)
+        self.lblBrowserLocation = tk.Label(self.frmBrowserLocation, text="Browser Location :")
+        self.lblBrowserLocation.pack(side="left")
+        self.txtBrowserLocation = tk.Entry(self.frmBrowserLocation)
+        self.txtBrowserLocation.pack(side="left", fill=tk.X, expand=True)
+        self.btnDefaultBrowser = tk.Button(self.frmBrowserLocation, text="...", command = self.onbtnDefaultBrowserClick)
+        self.btnDefaultBrowser.pack(side="left")
+        self.btnTryFind = tk.Button(self.frmBrowserLocation,text="Try to Find it...")
+        self.btnTryFind.pack(side="left")
+        self.frmBrowserLocation.pack(fill=tk.X, ipadx=2, ipady=2)
+
+    def OncbDefaulBrowserSelected(self, event):
+        print( event.widget.get())
+    
+    def onbtnDefaultBrowserClick(self):
+        print(tk.filedialog.askopenfile(initialdir = "/",title = "Select file",filetypes = (("executable files","*.exe"),("all files","*.*"))))
+
 if __name__ == "__main__":
     root = tk.Tk()
     frmOptions(root).pack(side="top", fill="both", expand=True)
